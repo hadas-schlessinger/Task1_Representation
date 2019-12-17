8794e2e3b4ba456690ad61ec0d67c1209ed5059a
Hadas Schlessinger <hadass@rezilion.com>
2019-12-17 11:44:50 +0200

    data structure

---
+++ data_presentation.py
@@ -0,0 +1,31 @@
+
+def get_default_parameters():
+
+    # Returns a dict containing the default experiment parameters
+    # It has several fields, each itself a dict of parameters for the various experiment stages
+    # These are ‘Split’, ‘Prepare’, ‘Train’, ‘Summary’, ‘Report’ (according to the needs)
+    # Each struct is sent to the relevant function (i.e. Params[‘Train’] is sent to Train(), etc.)
+    # Each experiment starts by configuring the experiment parameters:
+    # Calling GetDefaultParameters()
+    # Changing relevant parameters according to the specific experiments needs
+    # Do not keep hidden constants in the code (use parameters to set them)
+    pass
+
+
+def get_data(parameters):
+    # Loads the data and subsets it if required
+    # Puts the data in DandL[‘Data’], the labels in DandL[‘Labels’],
+    # In our case: Params include a path for the data and sub - setting parameters
+
+    data = 2
+    pass
+
+
+def train_split_data(TrainData, TestData, TrainLabels, TestLabels):
+    # Splits the data and labels according to a ratio defined in Params
+    # SplitData includes fields: TrainData, TestData, TrainLabels, TestLabels
+    pass
+
+
+def preper():
+    pass
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import os
 import numpy as np
-
+import data_presentation as dp
+import model
 
 def main():
-    pass
+    Params = dp.get_default_parameters()   # (experiment specific parameters override)
+    np.random.seed(0); # Seed
+    DandL = dp.get_data(Params[‘Data’])
+    SplitData = dp.train_split_data(DandL['Data'], DandL['Labels'], Params['Split']) # returns train data, test data, train labels and test labels
+    TrainDataRep = dp.prepare(SplitData['Train']['Data'], Params['Prepare'])
+    Model = train(TrainDataRep, SplitData['Train']['Labels'], Params['Train'])
+    TestDataRep = Prepare(SplitData['Test']['Data'], Params['Preapare'])
+    Results = Test(Model, TestDataRep)
+    Summary = Evaluate(Results, SplitData['Test']['Labels'], Params['Summary'])
+    ReportResults(Summary, Params['Report'])
 
 
-if __name__ == "__main__":
+
+    if __name__ == "__main__":
     main()
