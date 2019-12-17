a4ec81669b099287e2ea264345ef8e24d1e13cb2
NoyKalo <noykalo3@gmail.com>
2019-12-17 12:28:11 +0200

    evaluat anreport results

--- model.py
+++ model.py
@@ -7,9 +7,15 @@ def test():
     pass
 
 
-def evaluate():
+def evaluate(results,split_data, params):
+    # Compute the results statistics and return them as fields of Summary For classification these are:
+    # Most important: the error rate In our case also:
+    # Confusion matrix, the indices of the largest error images
     pass
 
 
-def report_results():
+
+def report_results(summary, params):
+    # Draws the results figures, reports results to the screen
+    # Saves the results to the results path, to a file named according to the experiment name or number (e.g. to Results\ResultsOfExp_xx.pkl)
     pass
